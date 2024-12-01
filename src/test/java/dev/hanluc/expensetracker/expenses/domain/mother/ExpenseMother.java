4a49a4ac0dd1ef1aaa59b1718a618caa93987fda
Jose Luceño <1618926+jalucenyo@users.noreply.github.com>
2024-12-01 17:20:39 +0100

    Seed example expenses

--- src/test/java/dev/hanluc/expensetracker/expenses/domain/mother/ExpenseMother.java
+++ src/test/java/dev/hanluc/expensetracker/expenses/domain/mother/ExpenseMother.java
@@ -3,21 +3,22 @@ package dev.hanluc.expensetracker.expenses.domain.mother;
 
 import dev.hanluc.expensetracker.common.domain.vo.Money;
 import dev.hanluc.expensetracker.expenses.domain.Expense;
+import dev.hanluc.expensetracker.expenses.domain.ExpenseBuilder;
 
 import java.time.OffsetDateTime;
 
 public class ExpenseMother {
 
   public static Expense random() {
-    return Expense.builder()
-      .amount(new Money(130L, 2))
-      .description("Expense 01")
-      .transactionDate(OffsetDateTime.now())
-      .paymentMethod("CASH")
-      .vendor("Amazon")
-      .recurrence("WEEKLY")
-      .notes("This is a note")
-      .build();
+    return new ExpenseBuilder()
+      .setAmount(new Money(130L, 2))
+      .setDescription("Expense 01")
+      .setTransactionDate(OffsetDateTime.now())
+      .setPaymentMethod("CASH")
+      .setVendor("Amazon")
+      .setRecurrence("WEEKLY")
+      .setNotes("This is a note")
+      .createExpense();
   }
 
 }
