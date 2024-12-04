8388220edbb932d4eb7f17511b7a9d64dd20eb71
Jose Luceño <1618926+jalucenyo@users.noreply.github.com>
2024-12-04 21:25:04 +0100

    feature: operation add money

--- src/main/java/dev/hanluc/expensetracker/common/domain/vo/Money.java
+++ src/main/java/dev/hanluc/expensetracker/common/domain/vo/Money.java
@@ -1,5 +1,6 @@
 package dev.hanluc.expensetracker.common.domain.vo;
 
+import dev.hanluc.expensetracker.budgets.domain.exception.BudgetException;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
@@ -12,4 +13,22 @@ public record Money(
   @NotNull
   Integer exponent
 
-) { }
+) {
+
+  public Money add(Money money) {
+    if (money == null) {
+      throw new BudgetException("The money to add cannot be null.");
+    }
+
+    if(this.exponent.equals(money.exponent)) {
+      return new Money(this.value + money.value, this.exponent);
+    }
+
+    long adjustedValue = (exponent > money.exponent)
+      ? money.value * (long) Math.pow(10, (this.exponent - money.exponent))
+      : value * (long) Math.pow(10, (money.exponent - this.exponent));
+
+    return new Money(money.value + adjustedValue, money.exponent);
+  }
+
+}
