8ef369edb0e6ce157290d898d75411a163e4a76b
Jose Luceño <1618926+jalucenyo@users.noreply.github.com>
2024-12-08 13:20:07 +0100

    refactor remove unused imports

--- src/main/java/dev/hanluc/expensetracker/expenses/infrastructure/api/advice/ExpenseTrackerExceptionAdvice.java
+++ src/main/java/dev/hanluc/expensetracker/expenses/infrastructure/api/advice/ExpenseTrackerExceptionAdvice.java
@@ -3,7 +3,6 @@ package dev.hanluc.expensetracker.expenses.infrastructure.api.advice;
 import dev.hanluc.expensetracker.common.ProblemDetailUtils;
 import dev.hanluc.expensetracker.common.domain.vo.ResultError;
 import dev.hanluc.expensetracker.expenses.domain.exception.ExpenseTrackerException;
-import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
@@ -11,8 +10,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
